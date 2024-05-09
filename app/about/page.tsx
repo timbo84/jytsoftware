fc97d664afa2d361509912b1274e3b5577f35196
timbo84 <timber110406@gmail.com>
2024-05-09 14:37:03 -0600

    updated bio on founders

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,12 +8,14 @@ export default function AboutPage() {
       <Navigation />
       <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
-          <h1 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-            about page
+          <h1 className="text-3xl font-bold tracking-tight  sm:text-4xl bg-gradient-to-r from-red-500 to-yellow-500 text-transparent bg-clip-text">
+            WHAT WE'RE ABOUT
           </h1>
+          
         </div>
+        
       </div>
-
+      <div className="w-full h-px bg-zinc-800" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={5000}
